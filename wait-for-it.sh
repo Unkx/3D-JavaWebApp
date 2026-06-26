a842ba830f049ee636232033813300a5b4ba8849
The unknvn <mitie931@gmail.com>
2026-06-26 19:59:09 +0200

    Fix: Add wait-for-it script to ensure database is ready before backend starts

---
+++ wait-for-it.sh
@@ -0,0 +1,18 @@
+@'
+#!/bin/bash
+# wait-for-it.sh - Wait for database to be ready
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "$DB_USERNAME" -d "3D-JavaApp" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
+'@ | Out-File -FilePath wait-for-it.sh -Encoding utf8
